7612a6950de1c4a5f49a88ffebb6b06c35ffdc29
Jay <Jay@DOTNETDADDY>
2025-08-27 14:37:17 +0530

    Added code comments

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddMemoryCache();
+
+builder.Services.AddHttpContextAccessor(); // Required UnoViewer
+builder.Services.AddMemoryCache(); // Required UnoViewer
 
 
 var app = builder.Build();
@@ -31,11 +32,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+// UnoViewer Start
 app.MapWhen(context => context.Request.Path.ToString().EndsWith("UnoImage.axd"),
-                appBranch =>
-                {
-                    appBranch.UseUnoViewer(new UnoViewOptions { UnSafe = false, ShowInfo = false });
-                }
-           );
+    appBranch =>
+    {
+        appBranch.UseUnoViewer(new UnoViewOptions { UnSafe = false, ShowInfo = false });
+    }
+);
+// UnoViewer End
 
 app.Run();
